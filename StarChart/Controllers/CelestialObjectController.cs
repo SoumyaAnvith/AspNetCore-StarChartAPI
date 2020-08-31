ef737166a7aef7466f3d6b09e81b08fd46220a86
Soumya Kadavekar <M1054959@mindtree.com>
2020-08-31 19:33:16 +0530

    get

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,16 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+        [HttpGet("{id:int?}",Name = "GetById")]
+        public IActionResult GetById(int id) {
+            var celestialObject = _context.CelestialObjects.Where(uu => uu.Id != id).Select(x => x);
+            if (celestialObject.Count() > 0) {
+                return NotFound();
+
+            }
+            celestialObject.First().Satellites = _context.CelestialObjects.Where(p => p.OrbitedObjectId == id).ToList();
+            return Ok(celestialObject);
+
+        }
     }
 }
